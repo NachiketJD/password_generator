d183205e8147d37d0450ca0041cb64f9244a13c8
Nachiket Jayant Deshpande <nachiketdeshpande2004@gmail.com>
2025-05-03 10:36:20 +0530

    F

--- main.go
+++ main.go
@@ -3,3 +3,4 @@ package main
 const lowercaseLetters = "abcdefghijklmnopqrstuvwxyz"
 const uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 const numbers = "0123456789"
+const specialChars = "!@#$%^&*-{};'.<>/?`"
